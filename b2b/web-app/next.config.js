55a67fca06ba34679f5a8c7b0d54133b3fa09081
Maduranga Siriwardena <maduranga.siriwardena@gmail.com>
2025-07-09 21:03:31 +0530

    Configure webpack aliases via tsconfig

--- b2b/web-app/next.config.js
+++ b2b/web-app/next.config.js
@@ -1,57 +1,36 @@
-/**
- * Copyright (c) 2022, WSO2 LLC. (https://www.wso2.com). All Rights Reserved.
- *
- * WSO2 LLC. licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+const path = require('path');
+const withFonts = require('next-fonts');
+const withLess = require('next-with-less');
+const tsconfig = require('./tsconfig.base.json');
 
-const { withNx } = require("@nrwl/next/plugins/with-nx");
-const withFonts = require("next-fonts");
-const withLess = require("next-with-less");
+const alias = Object.entries(tsconfig.compilerOptions.paths).reduce((acc, [key, value]) => {
+  const cleanedKey = key.replace(/\/*$/, '');
+  const cleanedValue = value[0].replace(/\/*$/, '');
+  acc[cleanedKey] = path.join(__dirname, cleanedValue);
+  return acc;
+}, {});
 
-const lessConfig = withLess({
-    lessLoaderOptions: {
-        lessOptions: {
-            strictMath: true
-        }
+const nextConfig = {
+  lessLoaderOptions: {
+    lessOptions: {
+      strictMath: true
     }
-});
+  },
+  webpack(config) {
+    config.resolve.alias = { ...config.resolve.alias, ...alias };
+    return config;
+  },
+  publicRuntimeConfig: {
+    adminRole: process.env.ADMIN_ROLE_NAME,
+    baseOrgUrl: process.env.BASE_ORG_URL,
+    baseUrl: process.env.BASE_URL,
+    meetingServiceUrl: process.env.MEETING_SERVICE_URL,
+    clientId: process.env.CLIENT_ID,
+    hostedUrl: process.env.HOSTED_URL,
+    personalizationServiceUrl: process.env.PERSONALIZATION_SERVICE_URL,
+    petManagementServiceUrl: process.env.PET_MANAGEMENT_SERVICE_URL,
+    sharedAppName: process.env.SHARED_APP_NAME
+  }
+};
 
-module.exports = withFonts({
-    webpack(config) {
-        return config;
-    }
-});
-
-const nextConfig = withNx({
-    nx: {
-        svgr: false
-
-    },
-    publicRuntimeConfig: {
-        adminRole: process.env.ADMIN_ROLE_NAME,
-        baseOrgUrl: process.env.BASE_ORG_URL,
-        baseUrl: process.env.BASE_URL,
-        meetingServiceUrl: process.env.MEETING_SERVICE_URL,
-        clientId: process.env.CLIENT_ID,
-        hostedUrl: process.env.HOSTED_URL,
-        personalizationServiceUrl: process.env.PERSONALIZATION_SERVICE_URL,
-        petManagementServiceUrl: process.env.PET_MANAGEMENT_SERVICE_URL,
-        sharedAppName: process.env.SHARED_APP_NAME
-    },
-    ...lessConfig
-});
-
-module.exports = nextConfig;
-
+module.exports = withFonts(withLess(nextConfig));
