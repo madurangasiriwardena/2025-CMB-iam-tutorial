12451ab0f350a613341a00d9f7d9e487be6ed700
Maduranga Siriwardena <maduranga.siriwardena@gmail.com>
2025-07-23 14:16:37 +0530

    Remove stylus dependency

--- b2b/web-app/app.jest.config.ts
+++ b2b/web-app/app.jest.config.ts
@@ -16,13 +16,12 @@
  * under the License.
  */
 
-/* eslint-disable */
 export default {
   displayName: "my-new-app",
   preset: "../../jest.preset.js",
   transform: {
-    "^(?!.*\\.(js|jsx|ts|tsx|css|json)$)": "@nrwl/react/plugins/jest",
-    "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["@nrwl/next/babel"] }]
+    // Use only babel-jest with next/babel preset for ts/js files
+    "^.+\\.[tj]sx?$": ["babel-jest", { presets: ["next/babel"] }]
   },
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   coverageDirectory: "../../coverage/apps/my-new-app"
